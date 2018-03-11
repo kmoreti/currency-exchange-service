6870107297adf6464261218adf7104a9431ac607
kmoreti <kleber.camargo@fatec.sp.gov.br>
2018-03-10 22:28:20 -0300

    Added some logging.

--- src/main/java/br/com/moreti/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/br/com/moreti/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -1,8 +1,11 @@
 package br.com.moreti.microservices.currencyexchangeservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.sampler.SamplerProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,5 +13,10 @@ public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
--- src/main/java/br/com/moreti/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/br/com/moreti/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package br.com.moreti.microservices.currencyexchangeservice.controller;
 
 import br.com.moreti.microservices.currencyexchangeservice.model.ExchangeValue;
 import br.com.moreti.microservices.currencyexchangeservice.repository.ExchangeValueRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +13,8 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private Environment environment;
 
@@ -25,7 +29,7 @@ public class CurrencyExchangeController {
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
-
+        logger.info("{}", exchangeValue);
         return exchangeValue;
     }
 }
